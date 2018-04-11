a8a536580c4d5d7094615f76e7c7fa913f5f3930
wojciech.drozd <wojciech.drozd@KRKPCC476302.clients.global.arup.com>
2018-04-11 17:09:34 +0200

    Data reader updated with updateDataTable method, other methodes modified

--- BusyForcast/src/DataReader.java
+++ BusyForcast/src/DataReader.java
@@ -13,13 +13,61 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class DataReader {
 	
-	// musi czytać wszystkie pliki w katalogu których nazwa to tylko cyferki i po kolei je sprawdzac
-	// musi stworzyc tablice z zespolami i gradami która bedzie aktualizowana po przejsciu kazdego zespolu w kazdym arkuszu
+	// bez sensu skomplikowane sa poki co dwie tabele - wymyslic cos innego
+
+	// musi czytać wszystkie pliki w katalogu których nazwa to tylko cyferki i po
+	// kolei je sprawdzac
+	// musi stworzyc tablice z zespolami i gradami która bedzie aktualizowana po
+	// przejsciu kazdego zespolu w kazdym arkuszu
 	public static void main(String[] args) {
+		// utworzenie tabel z danymi teamów
+		double[][] StruTable = new double[9][12];
+		double[][] PMTable = new double[9][12];
+		double[][] MechTable = new double[9][12];
+		double[][] PHTable = new double[9][12];
+		double[][] ElecTable = new double[9][12];
+		double[][] ConsTable = new double[9][12];
+		double[][] GeotTable = new double[9][12];
+		double[][] WaterTable = new double[9][12];
+		double[][] EnvTable = new double[9][12];
+		double[][] BridgeTable = new double[9][12];
+		double[][] RoadTable = new double[9][12];
+		double[][] AdminTable = new double[9][12];
+		double[][] OtherTable = new double[9][12];
+		// utworzenie docelowej tabeli z danymi
+		double[][] dataTable = new double[108][13];
+		// dodanie ID do docelowej tabeli z danymi
+		for (int i = 0; i < dataTable.length; i++) {
+			dataTable[i][0] = i + 1;
+		}
 		try {
 			Sheet dataSheet = createDataSheet("240849.xlsx");
-			printTeamTable(43, 52, 5, 17, dataSheet);
-
+			StruTable = printTeamTable(13, 22, 5, 17, dataSheet);
+			PMTable = printTeamTable(28, 37, 5, 17, dataSheet);
+			MechTable = printTeamTable(43, 52, 5, 17, dataSheet);
+			PHTable = printTeamTable(58, 67, 5, 17, dataSheet);
+			ElecTable = printTeamTable(78, 87, 5, 17, dataSheet);
+			ConsTable = printTeamTable(98, 107, 5, 17, dataSheet);
+			GeotTable = printTeamTable(113, 122, 5, 17, dataSheet);
+			WaterTable = printTeamTable(128, 137, 5, 17, dataSheet);
+			EnvTable = printTeamTable(143, 152, 5, 17, dataSheet);
+			BridgeTable = printTeamTable(158, 167, 5, 17, dataSheet);
+			RoadTable = printTeamTable(173, 181, 5, 17, dataSheet);
+			AdminTable = printTeamTable(193, 202, 5, 17, dataSheet);
+			OtherTable = printTeamTable(209, 218, 5, 17, dataSheet);
+			updateDataTable(dataTable,StruTable,0); // nie dziala!
+			updateDataTable(dataTable,PMTable,1);
+			updateDataTable(dataTable,MechTable,2);
+			
+			
+		//testowy wydruk tabeli z danymi
+			for (int i = 0; i < 108; i++) {
+				for (int j = 0; j < 13; j++) {
+					System.out.print(dataTable[i][j] + " ");
+				}
+				System.out.println();
+			}
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -27,6 +75,7 @@ public class DataReader {
 		}
 	}
 
+	// zwraca arkusz z podanego excela
 	public static Sheet createDataSheet(String fileName) throws IOException {
 		FileInputStream excelFile = new FileInputStream(new File(fileName));
 		Workbook wb = new XSSFWorkbook(excelFile);
@@ -34,19 +83,46 @@ public class DataReader {
 		return dataSheet;
 	}
 
-	public static void printTeamTable(int rowMin, int rowMax, int colMin, int colMax, Sheet dataSheet) {
+	// tworzy tabele z danymi dla konkretnego team u na podstawie zakresu komorek w
+	// excelu
+	public static double[][] printTeamTable(int rowMin, int rowMax, int colMin, int colMax, Sheet dataSheet) {
 		Cell currentCell;
+		double[][] teamTable = new double[9][12];
+		int countRow = 0;
+		int countCol = 0;
 		for (int i = rowMin; i < rowMax; i++) {
+			countCol = 0;
 			for (int j = colMin; j < colMax; j++) {
 				currentCell = dataSheet.getRow(i).getCell(j);
 				if (currentCell != null && currentCell.getCellTypeEnum() == CellType.NUMERIC) {
 					System.out.print(currentCell.getNumericCellValue() + " ");
+					teamTable[countRow][countCol] = currentCell.getNumericCellValue();
 				} else {
 					System.out.print("0.0 ");
+					teamTable[countRow][countCol] = 0;
 				}
+				countCol += 1;
+			}
+			System.out.println();
+			countRow += 1;
+		}
+		System.out.println();
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 12; j++) {
+				System.out.print(teamTable[i][j] + " ");
 			}
 			System.out.println();
 		}
 		System.out.println("check");
+		return teamTable;
+	}
+
+	// aktualizuje docelowa tabele z danymi w oparciu o dane team
+	public static void updateDataTable(double[][] dataTable, double[][] teamTable, int startRow) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 12; j++) {
+				dataTable[startRow + i][j + 1] = teamTable[i][j];
+			}
+		}
 	}
 }
